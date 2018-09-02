909ae08c87b004b99f65eddf88c9b9cfd0badc6c
José Antonio Álvarez <jaam@jaam.eu>
2018-09-02 22:32:15 +0200

    Initial Commit

---
+++ src/main/java/org/flowable/FlowableMinimalApp.java
@@ -0,0 +1,43 @@
+package org.flowable;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.flowable.job.service.impl.asyncexecutor.DefaultAsyncJobExecutor;
+
+/**
+ * @author Jose Antonio Alvarez
+ */
+public class FlowableMinimalApp {
+
+    public static void main(String[] args) {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        DefaultAsyncJobExecutor asyncJobExecutor = new DefaultAsyncJobExecutor();
+        cfg.setAsyncExecutor(asyncJobExecutor);
+        cfg.setAsyncExecutorActivate(false);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("simple_process.bpmn20.xml")
+                .deploy();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        runtimeService.startProcessInstanceByKey(processDefinition.getKey());
+    }
+
+}
---
+++ src/main/java/org/flowable/SimpleServiceTask.java
@@ -0,0 +1,13 @@
+package org.flowable;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+/**
+ * @author Jose Antonio Alvarez
+ */
+public class SimpleServiceTask implements JavaDelegate {
+    public void execute(DelegateExecution execution) {
+        System.out.println("Hello world!");
+    }
+}
